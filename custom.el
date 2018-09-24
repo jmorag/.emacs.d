20625dc1f6b4d1e0a7c83692d0318c199ab9f870
Joseph Morag <jm4157@columbia.edu>
2018-09-24 10:29:37 -0400

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,13 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(electric-indent-mode nil)
  '(evil-collection-setup-minibuffer t)
  '(package-selected-packages
    (quote
-    (shakespeare-mode smex ivy-hydra which-key rainbow-delimiters rainbow-delimeters hasky-stack hindent intero treemacs-projectile treemacs-evil treemacs ranger aggressive-indent aggressive-indent-mode evil-unimpaired evil-exchange evil-smartparens exec-path-from-shell smartparens evil-magit evil-vimish-fold company-flx eyebrowse haskell-mode yasnippet-snippets yasnippet evil-goggles expand-region evil-multiedit magithub flycheck counsel-projectile magit evil-commentary avy company helm evil-indent-textobject evil-surround counsel ivy doom-modeline powerline telephone-line dashboard general doom-themes evil-collection evil use-package)))
+    (eshell-git-prompt wgrep solarized-theme yaml-mode shakespeare-mode smex ivy-hydra which-key rainbow-delimiters rainbow-delimeters hasky-stack hindent intero treemacs-projectile treemacs-evil treemacs ranger aggressive-indent aggressive-indent-mode evil-unimpaired evil-exchange evil-smartparens exec-path-from-shell smartparens evil-magit evil-vimish-fold company-flx eyebrowse haskell-mode yasnippet-snippets yasnippet evil-goggles expand-region evil-multiedit magithub flycheck counsel-projectile magit evil-commentary avy company helm evil-indent-textobject evil-surround counsel ivy doom-modeline powerline telephone-line dashboard general doom-themes evil-collection evil use-package)))
  '(safe-local-variable-values
    (quote
-    ((haskell-process-use-ghci . t)
+    ((haskell-indent-spaces . 2)
+     (haskell-process-use-ghci . t)
      (haskell-indent-spaces . 4)
      (intero-stack-yaml . "/Users/josephmorag/Code/crossword-helper/stack.yaml")))))
 
